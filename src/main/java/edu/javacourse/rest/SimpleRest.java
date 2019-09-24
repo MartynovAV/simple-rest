7af6ce30638c1cab72ab4e166b5333e77098296c
MartynovAV <mcae@yandex.ru>
2019-09-24 14:13:11 +0300

    initial commit

---
+++ src/main/java/edu/javacourse/rest/SimpleRest.java
@@ -0,0 +1,18 @@
+package edu.javacourse.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.awt.*;
+
+@Path("/simple")
+public class SimpleRest {
+    @GET
+    @Path("/get")
+    @Produces(MediaType.APPLICATION_JSON)
+
+    public SimpleBean getBean(){
+        return new SimpleBean("First","Last");
+    }
+}
